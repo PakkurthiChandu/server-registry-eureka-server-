0a69cad31b1f230e5bdd12c16befc6ab7053954e
PakkurthiChandu <21mh1a4241@acoe.edu.in>
2025-08-05 16:56:43 +0530

    first commit

---
+++ src/test/java/com/chandu/server_registry/ServerRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chandu.server_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
