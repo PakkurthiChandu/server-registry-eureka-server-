e4bf534e072843808a09d2d1e32e1f9bbb011829
PakkurthiChandu <21mh1a4241@acoe.edu.in>
2025-08-06 09:54:24 +0530

    updated code

--- src/main/java/com/chandu/server_registry/ServerRegistryApplication.java
+++ src/main/java/com/chandu/server_registry/ServerRegistryApplication.java
@@ -7,9 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class ServerRegistryApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ServerRegistryApplication.class, args);
 	}
-
 }
